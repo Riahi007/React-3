95b7bc3b4b0e60d63c9097a7b7a1dd9bcb901fe9
Riahi <riahimouadh02@gmail.com>
2024-11-21 09:57:19 +0100

    ract 3

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        fullName: "John Doe",
+        bio: "A passionate software developer.",
+        imgSrc: "https://www.irvinetechcorp.com/wp-content/uploads/2020/09/shutterstock_696061426.jpeg",
+        profession: "Software Engineer"
+      },
+      show: false,
+      intervalTime: 0 
+    };
+    this.toggleShow = this.toggleShow.bind(this);
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState(prevState => ({
+        intervalTime: prevState.intervalTime + 1
+      }));
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  toggleShow() {
+    this.setState(prevState => ({
+      show: !prevState.show
+    }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Person Profile</h1>
+
+        <button onClick={this.toggleShow}>
+          {this.state.show ? 'Hide Profile' : 'Show Profile'}
+        </button>
+
+        {this.state.show && (
+          <div className="profile">
+            <img src={this.state.person.imgSrc} alt="Profile" />
+            <h2>{this.state.person.fullName}</h2>
+            <p>{this.state.person.bio}</p>
+            <p><strong>Profession:</strong> {this.state.person.profession}</p>
+          </div>
+        )}
+
+        <div>
+          <p>Time since last mounted: {this.state.intervalTime} seconds</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
